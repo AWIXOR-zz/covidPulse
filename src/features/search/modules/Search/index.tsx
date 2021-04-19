9e74d8c6dbbafc99caa237ed308d17e59649f984
awixor <elhd.me@gmail.com>
2021-04-19 16:07:30 +0000

    fix naming comvention

--- src/features/search/modules/Search/index.tsx
+++ src/features/search/modules/Search/index.tsx
@@ -9,8 +9,8 @@ import Typography from 'ui/components/Typography'
 import ProgressBar from 'react-bootstrap/ProgressBar'
 import items from 'data'
 import InputField from 'ui/components/InputField'
-import Mask from 'assets/icons/Mask'
-import HandSanitizer from 'assets/icons/Mask'
+// import Mask from 'assets/icons/Mask'
+import HandSanitizer from 'assets/icons/HandSanitizer'
 import Wine from 'assets/icons/Wine'
 import Talking from 'assets/icons/Talking'
 import Singing from 'assets/icons/Singing'
@@ -99,7 +99,7 @@ export default function Search() {
                 {`${item.score} / 10`}
               </Typography>
               <IconsList>
-                {item.mask ? <Mask /> : null}
+                {/* {item.mask ? <Mask /> : null} */}
                 {item.handSanitizer ? <HandSanitizer /> : null}
                 {item.alcohol ? <Wine /> : null}
                 {item.alcohol ? <Talking /> : null}
